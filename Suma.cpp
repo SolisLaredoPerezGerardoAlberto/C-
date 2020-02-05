a64b2184fd15795bfe7ff6be8e56a45f93ab761f
Alberto solis <60403045+SolisLaredoPerezGerardoAlberto@users.noreply.github.com>
2020-02-04 17:31:07 -0800

    Create Suma.cpp

---
+++ Suma.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+ 
+int main () 
+{
+    float n1, n2, suma;
+ 
+    cout << "Dime un numero: ";
+    cin >> n1;
+    cout << "Dime otro numero: ";
+    cin >> n2;
+    suma = n1 + n2;
+    cout << "Su suma es " << suma;
+ 
+    return 0;
+}
