cf22c43ae47831658db37facaca69637e4a0f799
Alberto solis <60403045+SolisLaredoPerezGerardoAlberto@users.noreply.github.com>
2020-02-04 17:32:32 -0800

    Create resta.cpp

---
+++ resta.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+ 
+int main () 
+{
+    float n1, n2, Resta;
+ 
+    cout << "Dime un numero: ";
+    cin >> n1;
+    cout << "Dime otro numero: ";
+    cin >> n2;
+    suma = n1 - n2;
+    cout << "Su Resta es " << Resta;
+ 
+    return 0;
+}
