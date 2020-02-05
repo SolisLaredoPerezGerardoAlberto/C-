3fcec27c446e9c70d54f50797057271217818e33
Alberto solis <60403045+SolisLaredoPerezGerardoAlberto@users.noreply.github.com>
2020-02-04 17:33:39 -0800

    Create Multiplicacion.cpp

---
+++ Multiplicacion.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+ 
+int main () 
+{
+    float n1, n2, Multi;
+ 
+    cout << "Dime un numero: ";
+    cin >> n1;
+    cout << "Dime otro numero: ";
+    cin >> n2;
+    suma = n1 + n2;
+    cout << "Su Multiplicacion es " << Multi;
+ 
+    return 0;
+}
